eacb5a22b577b5f05c5bbe62d9e48529da696282
SupLano <myemail@example.com>
2022-11-15 19:45:59 +0100

    Push 1

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
@@ -1,12 +1,39 @@
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+const AWSXRay = require('aws-xray-sdk')
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-import { createLogger } from '../utils/logger'
-import { TodoItem } from '../models/TodoItem'
-import { TodoUpdate } from '../models/TodoUpdate';
+// import { createLogger } from '../utils/logger'
+// import { TodoUpdate } from '../models/TodoUpdate';
+
+import { TodoItem } from "../models/TodoItem"
 
 const XAWS = AWSXRay.captureAWS(AWS)
 
-const logger = createLogger('TodosAccess')
+// const logger = createLogger('TodosAccess')
 
 // TODO: Implement the dataLayer logic
+
+const todosTable = process.env.TODOS_TABLE
+const docClient: DocumentClient = createDynamoDBClient()
+
+export async function createTodo(todo: TodoItem): Promise<TodoItem> {
+    await docClient.put({
+      TableName: todosTable,
+      Item: todo
+    }).promise()
+
+    return todo
+  }
+
+  function createDynamoDBClient() {
+    if (process.env.IS_OFFLINE) {
+      console.log('Creating a local DynamoDB instance')
+
+
+      return new XAWS.DynamoDB.DocumentClient({
+        region: 'localhost',
+        endpoint: 'http://localhost:8000'
+      })
+    }
+
+    return new XAWS.DynamoDB.DocumentClient()
+}
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -3,17 +3,38 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+// import * as uuid from 'uuid'
+// import { getUserId } from '../utils';
+// import { createTodo } from '../../businessLogic/todos'
+import { todoBuilder } from '../../helpers/todos'
+import { createTodo } from '../../helpers/todosAcess'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    console.log(newTodo)
     // TODO: Implement creating a new TODO item
+    // const todoId = uuid.v4()
 
-    return undefined
+    // const todo = {
+    //   todoId: todoId,
+    //   userId : getUserId(event),
+    //   "createdAt": new Date().toISOString(),
+    //   "done": false,
+    //   "attachmentUrl": "http://example.com/image.png",
+    //   ...newTodo
+    //  }
+    const todo = todoBuilder(newTodo, event)
+    const createdTodo = await createTodo(todo);
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        createdTodo
+      })
+    }
+  }
 )
-
 handler.use(
   cors({
     credentials: true
