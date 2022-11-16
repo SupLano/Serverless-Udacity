207d92e8ada197d33ede3da19cbb09459fe19286
SupLano <myemail@example.com>
2022-11-16 23:55:51 +0100

    test

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = '8ruf5frr2e'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.co/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-wnaj625375r7akzi.us.auth0.com',            // Auth0 domain
+  clientId: 'midMfe16FxIVfBUlqSzLRT5aq1uENw9X',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
